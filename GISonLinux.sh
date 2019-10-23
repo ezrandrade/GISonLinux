516feb693a5752f1a30a28b3997e364f87a9c9eb
Esdras Andrade <ezrandrade@gmail.com>
2019-10-23 15:36:45 -0300

    Inserção de programa
    Inserido link de download do gvSIG, atribuída permissão de execução como programa e comando de instalação do programa.

--- GISonLinux.sh
+++ GISonLinux.sh
@@ -5,7 +5,7 @@ URL_GOOGLE_EARTH_PRO="https://dl.google.com/dl/linux/direct/google-earth-pro-sta
 URL_SPRING="http://www.dpi.inpe.br/spring/download/bin/linux/Spring5_5_5_Port_Ubuntu1604_x64.tar.gz"
 URL_TERRAVIEW="http://www.dpi.inpe.br/terralib5/download/download.php?FileName=terralib-5.4.3-ubuntu-16.04.tar.gz"
 URL_DRAFTSIGHT="http://www.draftsight.com/download-linux-ubuntu"
-
+URL_GVSIG="http://downloads.gvsig.org/download/gvsig-desktop-testing/dists/2.4.0/builds/2850/gvSIG-desktop-2.4.0-2850-final-lin-x86_64-standard.run"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/GISonLinux"
 # ------------------------------------------------------------------ #
@@ -28,9 +28,14 @@ sudo apt install flatpak
 mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GOOGLE_EARTH_PRO"        -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_DRAFTSIGHT"              -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GVSIG"                   -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+
+## Instalando pacotes .run baixados na sessão anterior ##
+sudo chmod +x $DIRETORIO_DOWNLOADS/*.run
+sudo -i $DIRETORIO_DOWNLOADS/./*.run
 
 ## Instalando programas do repositório APT##
 sudo apt install saga -y
