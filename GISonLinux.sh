23bc4931913b9fa3f6467ac9a4c18e956e9de68d
Esdras Andrade <ezrandrade@gmail.com>
2019-10-29 17:25:19 -0300

    Software adicionado
    Adicionado o Programa GPSXSee de visualização de dados de GPS.

--- GISonLinux.sh
+++ GISonLinux.sh
@@ -6,6 +6,7 @@ URL_SPRING="http://www.dpi.inpe.br/spring/download/bin/linux/Spring5_5_5_Port_Ub
 URL_TERRAVIEW="http://www.dpi.inpe.br/terralib5/download/download.php?FileName=terralib-5.4.3-ubuntu-16.04.tar.gz"
 URL_DRAFTSIGHT="http://dl-ak.solidworks.com/nonsecure/draftsight/2019SP0/draftSight.deb"
 URL_GVSIG="http://downloads.gvsig.org/download/gvsig-desktop-testing/dists/2.4.0/builds/2850/gvSIG-desktop-2.4.0-2850-final-lin-x86_64-standard.run"
+URL_GPSXSEE="https://download.opensuse.org/repositories/home:/tumic:/GPXSee/Debian_10/amd64/gpxsee_7.16_amd64.deb"
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/GISonLinux"
 # ------------------------------------------------------------------ #
@@ -29,6 +30,7 @@ mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GOOGLE_EARTH_PRO"        -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_DRAFTSIGHT"              -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GVSIG"                   -P "$DIRETORIO_DOWNLOADS"
+wget -c "URL_GPSXSEE"                  -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
